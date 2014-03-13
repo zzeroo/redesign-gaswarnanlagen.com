43d605930bc5d686a12da504d3e03ded4ab21625
Stefan Müller <s.mueller@it.kls-glt.de>
2014-03-13 11:01:12 +0100

    Static pages extended

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ RedesignGaswarnanlagenCom::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
+  get "static_pages/contact"
   resources :users
 
   root 'static_pages#home'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) {"RA-GAS GmbH"}
+
   describe "Home page" do
     it "should have the content 'Redesign gaswarnanlagen.com'" do
         visit '/static_pages/home'
@@ -10,7 +12,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
         visit '/static_pages/home'
-        expect(page).to have_title("RA-GAS GmbH | Home")
+        expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -22,19 +24,31 @@ describe "StaticPages" do
 
     it "should have the right title" do
         visit '/static_pages/help'
-        expect(page).to have_title("RA-GAS GmbH | Hilfe")
+        expect(page).to have_title("#{base_title} | Hilfe")
     end
   end
 
   describe "About page" do
-    it "should have the content 'Über uns'" do
+    it "should have the content 'Firmenprofil'" do
         visit '/static_pages/about'
-        expect(page).to have_content('Über uns')
+        expect(page).to have_content('Firmenprofil')
     end
 
     it "should have the right title" do
         visit '/static_pages/about'
-        expect(page).to have_title("RA-GAS GmbH | Über uns")
+        expect(page).to have_title("#{base_title} | Firmenprofil")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Kontakt'" do
+        visit '/static_pages/contact'
+        expect(page).to have_content('Kontakt')
+    end
+
+    it "should have the right title" do
+        visit '/static_pages/contact'
+        expect(page).to have_title("#{base_title} | Kontakt")
     end
   end
 
