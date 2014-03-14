9b4f39a9a54db04e5a6318eec4dc3ea40d439ced
Stefan Müller <s.mueller@it.kls-glt.de>
2014-03-14 16:05:12 +0100

    Some more additions to User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,7 +30,7 @@ describe User do
 
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+      addresses = %w[user@foo..com user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
@@ -57,6 +57,16 @@ describe User do
     end
 
     it { should_not be_valid }
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAmPLE.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
   end
 
   describe "when password is not present" do
