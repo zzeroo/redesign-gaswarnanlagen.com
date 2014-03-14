43ec112ce032e58eb90929fdaf458a14824c01b8
Stefan Müller <s.mueller@it.kls-glt.de>
2014-03-14 12:22:29 +0100

    additional tweaks on rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,35 +4,55 @@ describe "StaticPages" do
 
   let(:base_title) {"RA-GAS GmbH"}
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   subject { page }
 
   describe "Home page" do
     before { visit root_path }
+    let(:heading)     { 'RA-GAS' }
+    let(:page_title)  { ''}
 
-    it { should have_content('RA-GAS') }
-    it { should have_title(full_title('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_title("| Home") }
   end
 
   describe "Help page" do
     before { visit help_path }
+    let(:heading)     { 'Hilfe' }
+    let(:page_title)  { 'Hilfe' }
 
-    it { should have_content('Hilfe')}
-    it { should have_title(full_title('Hilfe')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
+    let(:heading)     { 'Firmenprofil' }
+    let(:page_title)  { 'Firmenprofil' }
 
-    it { should have_content('Firmenprofil')}
-    it { should have_title(full_title('Firmenprofil')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
+    let(:heading)     { 'Kontakt' }
+    let(:page_title)  { 'Kontakt' }
 
-    it { should have_content('Kontakt')}
-    it { should have_title(full_title('Kontakt')) }
+    it_should_behave_like "all static pages"
   end
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "Home"
+    click_link "Hilfe"
+    expect(page).to have_title(full_title('Hilfe'))
+    click_link "Anmelden"
+    click_link "Über uns"
+    expect(page).to have_title(full_title('Firmenprofil'))
+    click_link "Kontakt"
+    expect(page).to have_title(full_title('Kontakt'))
+  end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,7 +7,7 @@ describe "UserPages" do
   describe "signup page" do
     before { visit signup_path }
 
-    it { should have_content('Anmeldung') }
+    it { should have_selector('h1', text: 'Anmeldung') }
     it { should have_title(full_title('Anmeldung')) }
   end
 end
