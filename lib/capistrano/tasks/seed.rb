9d3b327189a88315104dfc706b38eaa2f4815f25
Stefan Müller <s.mueller@it.kls-glt.de>
2014-05-15 12:54:54 +0200

    capistrano3 task

---
+++ lib/capistrano/tasks/seed.rb
@@ -0,0 +1,16 @@
+# http://stackoverflow.com/questions/7461985/how-to-seed-the-production-database-using-the-capistrano-gem
+#
+namespace :deploy do
+  desc 'Runs rake db:seed for SeedMigrations data'
+  task :seed => [:set_rails_env] do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:seed"
+        end
+      end
+    end
+  end
+
+  after 'deploy:migrate', 'deploy:seed'
+end
