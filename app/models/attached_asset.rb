f7a00232a47a67df08f59098b0911574a19c0523
Stefan Müller <s.mueller@it.kls-glt.de>
2014-05-21 17:05:38 +0200

    Skip Image Processing for Documents (docx, xls ...)

--- app/models/attached_asset.rb
+++ app/models/attached_asset.rb
@@ -6,4 +6,20 @@ class AttachedAsset < ActiveRecord::Base
                     styles: {thumbnail: "60x60#", small: "x100", medium: "300x300", big: "800x800" }
 
   do_not_validate_attachment_file_type :asset
+
+  before_post_process :skip_for_documents
+
+  def skip_for_documents
+    ! %w(
+      .xlsx   application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
+      .xltx   application/vnd.openxmlformats-officedocument.spreadsheetml.template
+      .potx   application/vnd.openxmlformats-officedocument.presentationml.template
+      .ppsx   application/vnd.openxmlformats-officedocument.presentationml.slideshow
+      .pptx   application/vnd.openxmlformats-officedocument.presentationml.presentation
+      .sldx   application/vnd.openxmlformats-officedocument.presentationml.slide
+      .docx   application/vnd.openxmlformats-officedocument.wordprocessingml.document
+      .dotx   application/vnd.openxmlformats-officedocument.wordprocessingml.template 
+      ).include?(asset_content_type)
+  end
+
 end
