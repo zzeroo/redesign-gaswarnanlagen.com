5dd19d034d7624115fa067d7501429a1cdbf8cfb
Stefan Müller <s.mueller@it.kls-glt.de>
2014-08-08 12:09:39 +0200

    Select Feld in Kategorie _fields
    Die Sortierung ist im Select Feld der Parent Kategorien nach :created_at, Damit stehen hoffentlich die ältesten (die Root Kategorien) ganz oben.
    Das muss def. überarbeitet werden. Am besten mit einer Drag n Drop Lösung ohne das beschissene Select Feld :)

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -2,7 +2,7 @@ module CategoriesHelper
   def categories_for_select
     # category| [category.name + "   '" + truncate(category.description, :length => 30) + "'", category.id, category.children.empty? ? "" : {class: 'bold'}] }
 
-    Category.all.collect do |c|
+    Category.all.order(:created_at).collect do |c|
       [
        c.parent.blank? ? c.name : c.name + "   '" + truncate(c.description, :length => 30) + "'", 
        c.id, 
