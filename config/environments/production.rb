449eda02e09785bb5a27ac3233f5c330094284b6
Stefan Müller <s.mueller@it.kls-glt.de>
2014-08-13 14:46:34 +0200

    Fix missings for last commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,7 @@ RedesignGaswarnanlagenCom::Application.configure do
 
 
 PAPERCLIP_STORAGE_OPTIONS = { :storage => :s3,
-                              :s3_credentials => Rails.root.join("config/s3_credentials.yml") }
+                              :s3_credentials => Rails.root.join("config/s3_credentials.yml"),
+                              :url  => ":s3_eu_url" }
 
 end
