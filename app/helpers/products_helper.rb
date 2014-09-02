784986c1be568727ae0816b852c1f08cd0f18d70
Stefan Müller <s.mueller@it.kls-glt.de>
2014-09-02 07:53:42 +0200

    Betriebsanleitungen Symbol dynamisch

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -19,20 +19,23 @@ module ProductsHelper
   end
 
   # Helper für das richtige Symbol, abhänig vom Datei Typ: attachable_type
-  def icon_link_to( a, link_title = "Download", style = "" )
+  # TODO: Optionen mit Options Hash gestalten
+  def icon_link_to( a, link_title = "Download", style = "", css_class = 'fa-2x' )
     case a.asset.content_type
     when "application/pdf"
       icon_file = 'file-pdf-o'
       link_title = link_title + ' PDF'
+      # TODO: Style nur setzen wenn nicht im options Hash vorhanden
       style = "color: darkred"
     when "application/vnd.openxmlformats-officedocument.wordprocessingml.document" 
       icon_file = 'file-word-o'
       link_title = link_title + ' DOCX'
+      # TODO: Style nur setzen wenn nicht im options Hash vorhanden
       style = "color: blue"
     else
       icon_file = 'file-text'
     end
 
-    link_to( icon( icon_file, '', class: 'fa-2x', title: link_title, style: style ), a.asset.url ) 
+    link_to( icon( icon_file, '', class: css_class, title: link_title, style: style ), a.asset.url ) 
   end
 end
