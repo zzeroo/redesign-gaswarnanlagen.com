3cdcbdd3a3a9e8e1444ef686923040551ed068e9
Stefan Müller <s.mueller@it.kls-glt.de>
2014-09-03 14:50:40 +0200

    Diverse Style Änderungen
    - 'mehr' Button
    - BDA Upload Button
    Außerdem die Sunspot Solr Speichereinstellungen verbessert.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -5,11 +5,11 @@ module ProductsHelper
     if product.short_text1.blank?
       truncate((highlight_it product.short_description, search), :length => 60)
     else
-      truncate((highlight_it product.short_text2, search), :length => 60)
+      truncate((highlight_it product.short_text1, search), :length => 60)
     end
   end
 
-  # Helper to construct the short_description 
+  # Helper to construct the description 
   def show_description(product, search)
     if product.short_text2.blank?
       truncate((highlight_it product.description, search), :length => 160)
