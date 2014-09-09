dbeb7015f21ceeaf252079017080ff70b61b47e3
Stefan Müller <s.mueller@it.kls-glt.de>
2014-09-09 13:04:54 +0200

    Letzte Migration entfernt und Sunspot config getuned

--- db/migrate/20140908080429_run_paperclip_refresh.rb
+++
@@ -1,7 +0,0 @@
-class RunPaperclipRefresh < ActiveRecord::Migration
-  def change
-    if Rails.env.development? || Rails.env.production? 
-      Rake::Task['paperclip:refresh:missing_styles'].invoke
-    end
-  end
-end
