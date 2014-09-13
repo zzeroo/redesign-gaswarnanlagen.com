8026864e2d86648b4ab292d702ba478dad6a1bee
Stefan Müller <s.mueller@it.kls-glt.de>
2014-09-13 13:28:41 +0200

    https://coveralls.i://coveralls.io Integration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,8 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'paperclip/matchers'
 require 'capybara/rspec'
-
+require 'coveralls'
+Coveralls.wear!
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
