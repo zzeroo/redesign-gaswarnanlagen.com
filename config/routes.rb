da9286ad5c8c265668219f87b18a3904537dfdb9
zzeroo <co@zzeroo.com>
2018-05-24 14:29:11 +0200

    Add Datenschutzerklärung nach der Datenschutz-Grundverordnung DSGVO

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ RedesignGaswarnanlagenCom::Application.routes.draw do
   match '/about',  to: 'static_pages#about',   via: 'get'
   match '/contact',to: 'static_pages#contact', via: 'get'
   match '/imprint',to: 'static_pages#imprint', via: 'get'
+  match '/privacy',to: 'static_pages#privacy', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
 end
